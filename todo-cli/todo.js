5dc6d760f3306db78e5506bbe58e8e234a89ebf7
KomalRamraoJadhav <127689127+KomalRamraoJadhav@users.noreply.github.com>
2023-04-12 15:44:07 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,87 +1,44 @@
+//eslint disable no-undef
+
+
 const todoList = () => {
-  all = []
+  let all = []
   const add = (todoItem) => {
     all.push(todoItem)
-  }
+  };
   const markAsComplete = (index) => {
     all[index].completed = true
-  }
+  };
 
   const overdue = () => {
+    const due = new Date();
+    return all.filter((task) => task.dueDate < due.toLocaleDateString("en-CA"));
     // Write the date check condition here and return the array
     // of overdue items accordingly.
-    let datecheck =[];
-    all.forEach((item) =>
-    {
-      if(item.dueDate===yesterday && item.completed===false)
-      datecheck.push(item);
-    })
-      return datecheck.reverse();
-    }
-  
-
+  };
   const dueToday = () => {
+    const due = new Date();
+    return all.filter((task) => task.dueDate == due.toLocaleDateString("en-CA"));
     // Write the date check condition here and return the array
     // of todo items that are due today accordingly.
-    let todaydate =[];
-    all.forEach((item) =>
-    {
-      if(item.dueDate===today)
-      todaydate.push(item);
-    })
-    return todaydate.reverse();
-  }
+  };
 
   const dueLater = () => {
+    const due = new Date();
+    return all.filter((task) => task.dueDate > due.toLocaleDateString("en-CA"));
     // Write the date check condition here and return the array
     // of todo items that are due later accordingly.
-    let laterdate =[];
-    all.forEach((item) =>
-    {
-      if(item.dueDate===tomorrow)
-      laterdate.push(item);
-    })
-    return laterdate.reverse();
-  
-  }
-
-  const toDisplayableList = (Arr) => {
+    
+  };
+  const toDisplayableList = (list) => {
+    return list
+      .map(
+        (todo) => `${todo.completed ? '[x]' : '[ ]'} ${todo.title} ${todo.dueDate === today ? '' : todo.dueDate}`,
+      )
+      .join('\n');
+  };
     // Format the To-Do list here, and return the output string
     // as per the format given above.
-    let listdisplay = [];
-    Arr.forEach((item,index) =>
-    {
-      if(item.dueDate===yesterday)
-      { 
-        if(item.completted===true)
-        {
-          listdisplay.push("[x]" + " " + item.title + " " + item.dueDate);
-        } else {
-           listdisplay.push("[ ]" + " " + item.title + " " + item.dueDate);
-        }
-      }
-
-      if (item.dueDate === today) {
-        delete item.dueDate;
-        if (item.completed === true)
-        {
-          listdisplay.push("[x]" + " " + item.title);
-        }
-        else{
-          listdisplay.push("[ ]" + " " + item.title);
-        }
-      }
-
-      if(item.dueDate === tomorrow) {
-        listdisplay.push("[ ]" + " " + item.title + " " + item.dueDate);
-      }
-    })
-    listdisplay.reverse();
-        let val = listdisplay.join("\n");
-        return val;
-     
-  }
-    
   return {
     all,
     add,
@@ -92,6 +49,7 @@ const todoList = () => {
     toDisplayableList
   };
 };
+
 
 module.exports = todoList;
 
